1f3ff927853f911df3aaa1bfa90dfebf05113887
Gabriel <gabriel@quickup.io>
2023-01-06 12:18:27 -0300

    desenvolve controle de sessão, adiciona endpoint para logout

---
+++ rest-express/src/endpoints/auth.ts
@@ -0,0 +1,84 @@
+import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+var sessions: Array<{ authenticationToken: string, userId: number }> = []
+
+export function createEndpointsToAutentication(app: any) {
+    app.post('/signup', async (req: Request, res: Response) => {
+        const user = await prisma.user.findFirst(
+            {
+              where: {
+                email: req.body.email,
+              }
+            }
+          )
+        
+        if(!user){
+            const createdUser = await prisma.user.create({
+                data: {
+                    email: req.body.email,
+                    password: req.body.password,
+                    name: req.body.name
+                }
+            })
+        
+            res.status(200).json({message: 'User created!'})
+        }
+    
+        res.status(401).json({message: 'User already exists'});
+    })
+    
+    app.post('/login', async (req: Request, res: Response) => {
+      const user = await prisma.user.findFirst(
+        {
+          where: {
+            email: req.body.email,
+          }
+        }
+      )
+    
+      if(user){
+        if(user?.password == req.body.password) {
+          var sessionsWithoutActualUser = sessions.filter(data => { return data.userId == user.id })
+
+          if(sessionsWithoutActualUser.length == 0){
+            const token = generateToken(10)
+            sessions.push({
+                authenticationToken: token,
+                userId: user.id
+            })
+
+            res.status(200).json({...user, autenticationToken: token})
+
+          } else {
+            res.status(401).json({message: "Usuário já logado"})
+          }
+          
+        } else 
+          res.json({message: "Senha ou email incorretos"})
+    
+      } else {
+        res.json({message: "Senha ou email incorretos"})
+      }
+      
+    })
+
+    app.post('/logout', async (req: Request, res: Response) => {
+        sessions = sessions.filter(data => { return data.authenticationToken != req.body.autenticationToken })
+
+        res.json({message: "Usuário deslogado"})
+    })
+    
+}
+
+function generateToken(length: number) {
+  
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var result = ''
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return result
+  }
