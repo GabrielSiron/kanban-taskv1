e79cafa1027386388dd8bfaad0b114109e38394c
M4RC0N3 <marcone@quickup.io>
2023-01-12 20:50:49 -0300

    complete callToActionSide

--- react/src/pages/sign-in/index.tsx
+++ react/src/pages/sign-in/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { AuthPage, CallToActionSide, AuthSide, MainTitle} from '../../styles/main-auth-structure';
-
+import { AuthPage, CallToActionSide, AuthSide, MainView, MainTitle, SubTitle, ImageContainer,
+PoweredContainer, PoweredTxt
+} from '../../styles/main-auth-structure';
+import Kanban from '../../assets/img/kanban.jpg';
+import QuickupLogo from '../../assets/img/quickup.svg'
 const SignIn = () => {
   return(
     <AuthPage>
       <CallToActionSide>
-        <MainTitle>Divide your tasks to conquer your results.</MainTitle>
+        <MainView>
+          <MainTitle>Divide your tasks to conquer your results.</MainTitle>
+          <SubTitle>The solution you need!</SubTitle>
+          <PoweredContainer>
+            <PoweredTxt>Powered by</PoweredTxt>
+            <img src={QuickupLogo} width={121} height={25} alt="Quickup logo" draggable={false}/>
+          </PoweredContainer>
+        </MainView>
+        <ImageContainer>
+          <img src={Kanban} width={1686} height={1125} alt='kanban board'/>
+        </ImageContainer>
       </CallToActionSide>
       <AuthSide></AuthSide>
     </AuthPage>
--- react/src/styles/main-auth-structure.ts
+++ react/src/styles/main-auth-structure.ts
@@ -6,11 +6,62 @@ export const AuthPage = styled.div`
     height: 100vh;
     grid-template-columns: 1fr 1fr;
 `;
+
 export const CallToActionSide = styled.section`
+    position: relative;
 `;
+
+export const MainView = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    z-index: 10;
+    backdrop-filter: blur(10px);
+    background-color: rgba(27, 31, 69, .56);
+    box-sizing: border-box;
+    padding: 85px 20px 20px 20px;
+`;
+
 export const MainTitle = styled.h1`
+    max-width: 593px;
+    font-size: 3.5rem;
+    text-align: center;
+    color: #fff;
     font-family: 'inter-bold';
 `;
+
+export const SubTitle = styled.h2`
+    font-size: 1.3rem;
+    color: #fff;
+    font-family: 'inter-regular';
+    margin-top: 20px;
+`
 export const AuthSide = styled.section`
     
+`;
+
+export const ImageContainer = styled.div`
+    position: absolute;
+    z-index: 5;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+`;
+
+
+export const PoweredContainer = styled.div`
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 20px;
+`;
+
+export const PoweredTxt = styled.div`
+    color: #fff;
+    font-size: 1.3rem;
+    margin-right: 10px;
+    font-family: 'inter-semibold';
 `;
